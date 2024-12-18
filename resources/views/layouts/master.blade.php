ab37fa3db072d5edef9338bb8047cc253d699b48
ReyyuAlter <rewsew10@gmail.com>
2024-12-18 20:22:39 +0800

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+@include('layouts.header')
+
+@include('layouts.navbar')
+
+@include('layouts.sidebar')
+
+@include('layouts.content')
+
+@include('layouts.footer')
+  
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div>
+
+ 
+
+ 
+
+
+  
